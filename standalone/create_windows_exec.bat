16a680a138d69a22b1316105274fbdb4fd13a731
1234ab <nevenincs007@gmail.com>
2023-02-05 22:36:42 +0100

    finetune win script

--- standalone/create_windows_exec.bat
+++ standalone/create_windows_exec.bat
@@ -3,5 +3,5 @@
 python -m venv virt_env
 virt_env/Scripts/activate.bat
 python -m pip install galaxy-jukebox-gui nuitka ordered-set zstandard
-python -m nuitka --onefile --enable-plugin=pyqt5 --include-package-data=galaxy_jukebox_gui --output-filename=galaxy-jukebox-gui.bin standalone.py
+python -m nuitka --onefile --enable-plugin=pyqt5 --include-package-data=galaxy_jukebox_gui --output-filename=galaxy-jukebox-gui.exe --disable-console standalone.py
 deactivate
