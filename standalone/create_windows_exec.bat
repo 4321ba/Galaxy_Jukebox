8e104bacf75e303d3e0fad11fe149961d4f96270
1234ab <nevenincs007@gmail.com>
2023-02-05 18:48:19 +0100

    add windows script

---
+++ standalone/create_windows_exec.bat
@@ -0,0 +1,7 @@
+:: probably wiser to paste the lines one by one
+
+python -m venv virt_env
+virt_env/Scripts/activate.bat
+python -m pip install galaxy-jukebox-gui nuitka ordered-set zstandard
+python -m nuitka --onefile --enable-plugin=pyqt5 --include-package-data=galaxy_jukebox_gui --output-filename=galaxy-jukebox-gui.bin standalone.py
+deactivate
