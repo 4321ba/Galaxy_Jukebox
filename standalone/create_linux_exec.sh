3c2ffd577a0ffa554eb5f80b78f83726a734075b
1234ab <nevenincs007@gmail.com>
2023-02-05 18:34:20 +0100

    add standalone executable creator using nuitka

---
+++ standalone/create_linux_exec.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+rm -r standalone.build
+rm -r standalone.dist
+rm -r standalone.onefile-build
+rm -r virt_env
+python3 -m venv virt_env
+source virt_env/bin/activate
+python3 -m pip install galaxy-jukebox-gui nuitka ordered-set zstandard
+python3 -m nuitka --onefile --enable-plugin=pyqt5 --include-package-data=galaxy_jukebox_gui --output-filename=galaxy-jukebox-gui.bin standalone.py
+deactivate
---
+++ standalone/standalone.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from galaxy_jukebox_gui.gui import main
+
+if __name__ == '__main__':
+    main()
